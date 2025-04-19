d961e9a420a5598f4b4cefa4734f459c154a542c
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2025-04-19 13:33:18 +0500

    updated navbar

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -43,8 +43,7 @@ export const DATA = {
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
-    { href: "#", icon: CodeIcon, label: "Projects" },
-    { href: "#", icon: PencilLine, label: "work" },
+
   ],
   contact: {
     email: "muneebsurrehman@gmail.com",
