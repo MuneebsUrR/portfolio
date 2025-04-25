01ce12e18d5f8cb6a33048936a223548ff83746b
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2025-04-26 02:28:07 +0500

    headline changed

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,7 +10,7 @@ export const DATA = {
   description:
     "Software Developer | DevOps Engineer. I love building things and helping people.",
   summary:
-    "I am a final-year student at [FAST National University](https://www.nu.edu.pk/) with two years of experience in building Apps and Websites. Currently, I am also expanding my skills by learning AI and machine learning. My academic journey has equipped me with a strong foundation in both frontend and backend development, and I am excited to apply my skills to real-world projects and contribute to the tech community.",
+    "I am a final-year student at [FAST National University](https://www.nu.edu.pk/) with two years of experience in building Apps and Web Applications. Currently, I am also expanding my skills like AI automation, DevOps, Cloud Engineering. My academic journey has equipped me with a strong foundation in both frontend and backend development, and I am excited to apply my skills to real-world projects and contribute to the tech community.",
   avatarUrl: "/mainpic.jpeg",
   skills: [
     "React",
